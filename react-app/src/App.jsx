1f35b5c99c5cdffee2e2ff09ff00c06e851bf3dd
Alan Almeida <alandavidalmeida@gmail.com>
2024-01-04 18:01:54 -0300

    FEATURE#6-footer

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,18 +6,46 @@ import Services from "./Components/Services"
 import Contact from "./Components/Contact"
 import Footer from "./Components/Footer";
 import './index.css'
+import { useEffect, useState } from "react"
+import { BallTriangle } from "react-loader-spinner"
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+    useEffect(() =>{
+      setLoading(true);
+      setTimeout(() =>{
+        setLoading(false);
+      }, 1000)
+    },[])
   return (
-    <div className = "bg-slate-900">
-      <Nav />
-      <Banner />
-      <About />
-      <Services />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+      <>
+        {
+          loading ?
+          <div className="bg-slate-900 h-[100vh] flex justify-center items-center">
+          <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#d946ef"
+          ariaLabel="ball-triangle-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          />
+          </div>
+          :
+
+          <div className = "bg-slate-900">
+          <Nav />
+          <Banner />
+          <About />
+          <Services />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+        }
+      </>
   )
 }
 
